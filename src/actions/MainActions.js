0ea9866150b78a97cb91ca75b88338a0a00cecbd
kimerran <mh.neri@gmail.com>
2020-03-14 22:03:58 +0800

    try workaround for cached json

--- src/actions/MainActions.js
+++ src/actions/MainActions.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const loadLinks = () => async (dispatch) => {
-  const { data } = await axios.get('https://raw.githubusercontent.com/kimerran/covid19ph.net/master/src/data/main_links.json')
+  const { data } = await axios.get(`https://raw.githubusercontent.com/kimerran/covid19ph.net/master/src/data/main_links.json?x=${+new Date()}`)
   dispatch({ type: 'SET_LINKS', data })
 }
 
